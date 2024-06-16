87f0c5283e0a16036784c6f4d39261e5e3c2421c
Aditya Prajapati <adityaprajapati2901@gmail.com>
2024-06-16 13:31:32 +0530

    Update firebase.js

--- authentication/firebase/firebase.js
+++ authentication/firebase/firebase.js
@@ -5,13 +5,7 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNtxSv5osVW12POUzCXxNIJUIP1av5amc",
-  authDomain: "kajri-appliction.firebaseapp.com",
-  projectId: "kajri-appliction",
-  storageBucket: "kajri-appliction.appspot.com",
-  messagingSenderId: "839198250926",
-  appId: "1:839198250926:web:cea10826232615c7af5a3f",
-  measurementId: "G-NNCQCRGXHW"
+
 };
 const app = initializeApp(firebaseConfig);
 
